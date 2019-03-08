da5713143fab7641ca08215db795adde790c4e66
albertgoncalves <goncalves.albert@gmail.com>
2019-03-07 20:46:38 -0500

    Variadic functions!

--- func/main.go
+++ func/main.go
@@ -6,6 +6,14 @@ import (
     "time"
 )
 
+func pipe(x int, fs ...func(int) int) int {
+    for _, f := range fs {
+        fmt.Println(x)
+        x = f(x)
+    }
+    return x
+}
+
 func add(a int) func(int) int {
     var f = func(b int) int {
         return a + b
@@ -13,18 +21,20 @@ func add(a int) func(int) int {
     return f
 }
 
-var gen = func(r *rand.Rand, k int) int {
+func gen(r *rand.Rand, k int) int {
     return r.Intn(k)
 }
 
 func main() {
     var seed int64 = time.Now().UnixNano()
     var r *rand.Rand = rand.New(rand.NewSource(seed))
-
-    const a, b = 100, 200
-    var c int = gen(r, b)
-    var d int = add(a)(b)
-    var e int = add(c)(d)
-
-    fmt.Println(e)
+    const a, b, c, d = 100, 200, 300, 400
+    y := pipe(
+        gen(r, a),
+        add(a),
+        add(b),
+        add(c),
+        add(d),
+    )
+    fmt.Println(y)
 }

4498da428b7fe860edb3143a3cb63880d4401bba
albertgoncalves <goncalves.albert@gmail.com>
2019-09-13 22:55:37 -0400

    better highlight concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -4,8 +4,20 @@ import (
     "bufio"
     "fmt"
     "os"
+    "strings"
     "time"
 )
+
+func deliver(response chan<- string, payload *string, n int) {
+    time.Sleep(time.Duration(n) * time.Second)
+    var m int
+    if n < 2 {
+        m = 1
+    } else {
+        m = n - 1
+    }
+    response <- fmt.Sprintf("%s%s", strings.Repeat("\t", m), *payload)
+}
 
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
@@ -14,13 +26,11 @@ func main() {
         for {
             scanner.Scan()
             payload := scanner.Text()
-            go func() {
-                time.Sleep(3 * time.Second)
-                response <- payload
-            }()
+            go deliver(response, &payload, 2)
+            go deliver(response, &payload, 4)
         }
     }()
     for {
-        fmt.Println("\t", <-response)
+        fmt.Println(<-response)
     }
 }

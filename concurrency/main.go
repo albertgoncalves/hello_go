d2c234c4693429ca00c4dccf5517e808f15ff1d5
albertgoncalves <goncalves.albert@gmail.com>
2019-09-13 23:05:16 -0400

    this is better

--- concurrency/main.go
+++ concurrency/main.go
@@ -4,33 +4,32 @@ import (
     "bufio"
     "fmt"
     "os"
-    "strings"
     "time"
 )
 
-func deliver(response chan<- string, payload *string, n int) {
+func deliver(response chan<- string, payload string, n int) {
     time.Sleep(time.Duration(n) * time.Second)
-    var m int
-    if n < 2 {
-        m = 1
-    } else {
-        m = n - 1
-    }
-    response <- fmt.Sprintf("%s%s", strings.Repeat("\t", m), *payload)
+    response <- payload
 }
 
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
-    response := make(chan string)
+    a := make(chan string)
+    b := make(chan string)
     go func() {
         for {
             scanner.Scan()
             payload := scanner.Text()
-            go deliver(response, &payload, 2)
-            go deliver(response, &payload, 4)
+            go deliver(a, payload, 2)
+            go deliver(b, payload, 4)
         }
     }()
     for {
-        fmt.Println(<-response)
+        select {
+        case response := <-a:
+            fmt.Println("\t", response)
+        case response := <-b:
+            fmt.Println("\t\t", response)
+        }
     }
 }
